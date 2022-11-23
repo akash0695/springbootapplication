df4070400e3530f72399f2833b7439968c23669b
Akash Gupta <110096505+akash0695@users.noreply.github.com>
2022-11-24 00:23:07 +0530

    Update MyController.java

--- src/main/java/com/springboot/springboot/controller/MyController.java
+++ src/main/java/com/springboot/springboot/controller/MyController.java
@@ -7,20 +7,14 @@ import com.springboot.springboot.services.FormService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 import com.springboot.springboot.entities.Course;
 import com.springboot.springboot.services.CourseService;
 
 @RestController
+@RequestMapping("/form/v2")
 public class MyController {
 	
 	@Autowired
