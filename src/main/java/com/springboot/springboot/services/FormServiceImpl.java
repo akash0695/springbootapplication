8c9f34ae396d38ca2e18dc0aa7becc53c4b547e2
Akash Gupta <akashpro0295@gmail.com>
2022-12-19 14:05:59 +0530

    Added jwt Spring Security for the Access and now its working

--- src/main/java/com/springboot/springboot/services/FormServiceImpl.java
+++ src/main/java/com/springboot/springboot/services/FormServiceImpl.java
@@ -7,6 +7,7 @@ import com.springboot.springboot.entities.FormData;
 import java.util.Iterator;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
 @Service
