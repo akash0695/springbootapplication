5449ebce9098f329e29416214fea4ab12a8fd8cf
akash0695 <akashtech0295@gmail.com>
2023-09-10 23:42:59 +0530

    new branch changes

--- src/main/java/com/springboot/springboot/config/WebSecurityConfig.java
+++ src/main/java/com/springboot/springboot/config/WebSecurityConfig.java
@@ -26,6 +26,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private UserDetailsService jwtUserDetailsService;
+	
 
 	@Autowired
 	private JwtRequestFilter jwtRequestFilter;
